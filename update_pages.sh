0f3035ab2325694d858f30d4cdd12b9165c76f21
Pål-Erik Martinsen <palmarti@cisco.com>
2013-11-26 20:57:39 +0100

    Updated script to reflect new "sharing" policy between master and gh-branch

--- update_pages.sh
+++ update_pages.sh
@@ -2,12 +2,6 @@
 
 ./createdrafts.sh
 
-git add Drafts
-git commit -a -m "Sync Drafts folder with source xml files"
-
-
-# Switch to gh-pages branch to sync it with master
-###################################################################
 git checkout gh-pages
 
 ./update.sh
